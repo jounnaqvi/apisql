d8256fe9e82f37b08ad3c1a0a7605556d2f3d2bb
jounnaqvi <142839126+jounnaqvi@users.noreply.github.com>
2025-03-14 20:58:21 +0530

    git init

--- config/db.js
+++ config/db.js
@@ -1,23 +1,20 @@
 const mysql = require('mysql2');
-require('dotenv').config();
 
-const pool = mysql.createPool({
+require("dotenv").config();
+
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10, // Number of connections allowed
-  queueLimit: 0
 });
 
-pool.getConnection((err, connection) => {
+connection.connect((err) => {
   if (err) {
     console.error("Database connection failed: " + err.message);
-  } else {
-    console.log("Connected to MySQL database");
-    connection.release(); // Release the connection after checking
+    return;
   }
+  console.log("Connected to MySQL database");
 });
 
-module.exports = pool.promise(); // Export pool with promise support
+module.exports = connection;
